97d5dd85cb574ec213c852ce0dd55604f0f23294
Pierre-Olivier Maugis <pierre-olivier.maugis@klaxit.com>
2023-06-01 11:17:41 +0200

    Initial Commit

---
+++ Aike/AikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AikeApp.swift
+//  Aike
+//
+//  Created by Pierre-Olivier Maugis on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
